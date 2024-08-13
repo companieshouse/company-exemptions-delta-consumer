12b0d7414abe33027b8a3254b3d91233670d9d2f
sthompsonCH <sthompson@companieshouse.gov.uk>
2024-08-13 12:45:22 +0100

    DSND-2881: Remove embedded kafka from cucumber

--- src/itest/java/uk/gov/companieshouse/exemptions/delta/Configuration.java
+++ src/itest/java/uk/gov/companieshouse/exemptions/delta/Configuration.java
@@ -4,21 +4,27 @@ import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @CucumberContextConfiguration
 @AutoConfigureMockMvc
-@SpringBootTest(classes = Application.class)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
-@EmbeddedKafka(
-        topics = {"company-exemptions-delta", "company-exemptions-delta-company-exemptions-delta-consumer-retry",
-                "company-exemptions-delta-company-exemptions-delta-consumer-error", "company-exemptions-delta-company-exemptions-delta-consumer-invalid"},
-        controlledShutdown = true,
-        partitions = 1
-)
+@SpringBootTest
 @Import(TestConfig.class)
 @ActiveProfiles("integration_tests")
 public class Configuration {
+
+    public static final KafkaContainer kafkaContainer = new KafkaContainer(
+            DockerImageName.parse("confluentinc/cp-kafka:latest"));
+
+    @DynamicPropertySource
+    static void props(DynamicPropertyRegistry registry) {
+        registry.add("spring.kafka.bootstrap-servers", kafkaContainer::getBootstrapServers);
+        kafkaContainer.start();
+    }
+
 }
