03aea2689737ee54eebb0dbf78c25ac176b38942
Terrance Thomas <tthomas1@companieshouse.gov.uk>
2024-12-19 11:35:54 +0000

    Updated module version

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -19,7 +19,7 @@ terraform {
 }
 
 module "ecs-service" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.289"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.296"
 
 
   # Environmental configuration
