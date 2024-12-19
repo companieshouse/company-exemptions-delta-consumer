23bf39bc59684d59a022c69fdf4f64e5533fa2e6
Terrance Thomas <tthomas1@companieshouse.gov.uk>
2024-12-19 11:06:04 +0000

    Update module version

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -67,7 +67,7 @@ module "ecs-service" {
 }
 
 module "secrets" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.289"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.296"
 
   name_prefix = "${local.service_name}-${var.environment}"
   environment = var.environment
