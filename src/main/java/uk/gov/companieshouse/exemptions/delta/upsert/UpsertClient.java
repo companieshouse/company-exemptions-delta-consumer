24896f66cdd50d64577989fa8c3cde3339a05028
Dan Toulcher <dtoulcher@companieshouse.gov.uk>
2025-01-09 12:05:31 +0000

    feat: added Mockito as an agent to remove warning
    JU-1177

--- src/main/java/uk/gov/companieshouse/exemptions/delta/upsert/UpsertClient.java
+++ src/main/java/uk/gov/companieshouse/exemptions/delta/upsert/UpsertClient.java
@@ -1,14 +1,13 @@
 package uk.gov.companieshouse.exemptions.delta.upsert;
 
 import java.util.function.Supplier;
+
 import org.springframework.stereotype.Component;
+
 import uk.gov.companieshouse.api.InternalApiClient;
 import uk.gov.companieshouse.api.error.ApiErrorResponseException;
 import uk.gov.companieshouse.api.handler.exception.URIValidationException;
-import uk.gov.companieshouse.exemptions.delta.Application;
 import uk.gov.companieshouse.exemptions.delta.ResponseHandler;
-import uk.gov.companieshouse.logging.Logger;
-import uk.gov.companieshouse.logging.LoggerFactory;
 
 ;
 
