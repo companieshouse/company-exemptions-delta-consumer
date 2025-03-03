e5913c380abde2cd6f7f9f5aba2971dda6432011
mturner-CH <mturner2@companieshouse.gov.uk>
2025-03-03 16:54:31 +0000

    DSND-2874: Implement DataMapHolder

--- src/main/java/uk/gov/companieshouse/exemptions/delta/kafka/MessageLoggingAspect.java
+++ src/main/java/uk/gov/companieshouse/exemptions/delta/kafka/MessageLoggingAspect.java
@@ -1,7 +1,9 @@
 package uk.gov.companieshouse.exemptions.delta.kafka;
 
-import java.util.HashMap;
-import java.util.Map;
+import static uk.gov.companieshouse.exemptions.delta.CompanyExemptionsDeltaConsumerApplication.NAMESPACE;
+
+import java.util.Optional;
+import java.util.UUID;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
@@ -10,7 +12,9 @@ import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 import uk.gov.companieshouse.delta.ChsDelta;
+import uk.gov.companieshouse.exemptions.delta.logging.DataMapHolder;
 import uk.gov.companieshouse.logging.Logger;
+import uk.gov.companieshouse.logging.LoggerFactory;
 
 /**
  * Logs message details before and after it has been processed by
@@ -30,33 +34,31 @@ public class MessageLoggingAspect {
 
     private static final String LOG_MESSAGE_RECEIVED = "Processing delta";
     private static final String LOG_MESSAGE_PROCESSED = "Processed delta";
-
-    private final Logger logger;
-
-    public MessageLoggingAspect(Logger logger) {
-        this.logger = logger;
-    }
+    private static final Logger LOGGER = LoggerFactory.getLogger(NAMESPACE);
 
     @Before("execution(* uk.gov.companieshouse.exemptions.delta.kafka.CompanyExemptionsDeltaConsumer.consume(..))")
     void logBeforeConsumer(JoinPoint joinPoint) {
-        logMessage(LOG_MESSAGE_RECEIVED, (Message<?>)joinPoint.getArgs()[0]);
+        Message<?> incomingMessage = (Message<?>)joinPoint.getArgs()[0];
+        ChsDelta payload = (ChsDelta)incomingMessage.getPayload();
+
+        DataMapHolder.initialise(Optional.ofNullable(payload.getContextId())
+                .orElse(UUID.randomUUID().toString()));
+
+        String topic = (String)incomingMessage.getHeaders().get(KafkaHeaders.RECEIVED_TOPIC);
+        Integer partition = (Integer)incomingMessage.getHeaders().get(KafkaHeaders.RECEIVED_PARTITION);
+        Long offset = (Long)incomingMessage.getHeaders().get(KafkaHeaders.OFFSET);
+
+        DataMapHolder.get()
+                .topic(topic)
+                .partition(partition)
+                .offset(offset);
+
+        LOGGER.info(LOG_MESSAGE_RECEIVED, DataMapHolder.getLogMap());
     }
 
     @After("execution(* uk.gov.companieshouse.exemptions.delta.kafka.CompanyExemptionsDeltaConsumer.consume(..))")
     void logAfterConsumer(JoinPoint joinPoint) {
-        logMessage(LOG_MESSAGE_PROCESSED, (Message<?>)joinPoint.getArgs()[0]);
-    }
-
-    private void logMessage(String logMessage, Message<?> incomingMessage) {
-        Map<String, Object> logData = new HashMap<>();
-        ChsDelta payload = (ChsDelta)incomingMessage.getPayload();
-        String topic = (String)incomingMessage.getHeaders().get(KafkaHeaders.RECEIVED_TOPIC);
-        Integer partition = (Integer)incomingMessage.getHeaders().get(KafkaHeaders.RECEIVED_PARTITION);
-        Long offset = (Long)incomingMessage.getHeaders().get(KafkaHeaders.OFFSET);
-        logData.put("contextId", payload.getContextId());
-        logData.put("topic", topic);
-        logData.put("partition", partition);
-        logData.put("offset", offset);
-        logger.debug(logMessage, logData);
+        LOGGER.info(LOG_MESSAGE_PROCESSED, DataMapHolder.getLogMap());
+        DataMapHolder.clear();
     }
 }
